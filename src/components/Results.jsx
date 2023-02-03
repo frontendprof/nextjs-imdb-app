b07c3fea9ed8cbe8aa5f768aac6339b10b942d63
cyberweb8 <abdumalikshareef@gmail.com>
2023-02-04 02:09:43 +0500

    feat: fetch data from tmdb api

---
+++ src/components/Results.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ({ results }) {
+  return (
+    <>
+      {results.map((item) => (
+        <div>
+          {item.original_title ? item.original_title : item.original_name}
+        </div>
+      ))}
+    </>
+  );
+}
