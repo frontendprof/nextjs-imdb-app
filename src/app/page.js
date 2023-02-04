4a23af1d312bb0efadf0b08ca40eef16171ffefa
cyberweb8 <abdumalikshareef@gmail.com>
2023-02-05 03:34:08 +0500

    fix: change revalidate text value

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default async function Home({ searchParams }) {
     `https://api.themoviedb.org/3/${
       genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'
     }?api_key=${API_KEY}&language=en-US&page=1`,
-    { nex: { revalidate: 10000 } }
+    { next: { revalidate: 10000 } }
   );
   if (!res.ok) {
     throw new Error('Failed to fetch data');
